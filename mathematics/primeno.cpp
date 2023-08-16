68c52fdd589c9c6e499d2c1b30d21f9910965545
ADITYAPRASADSAHOOS1200 <apsahoo5555@gmail.com>
2023-08-16 20:14:01 +0530

    final commit

---
+++ mathematics/primeno.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isprime(int n){
+if(n==1){
+    return false;
+}
+for(int i=2;i*i<=n;i++){
+    if(n % i == 0)
+     return false;
+}
+return true;
+}
+
+
+
+int main()
+{
+    int n=37;
+    cout<<isprime(n);
+    return 0;
+}
